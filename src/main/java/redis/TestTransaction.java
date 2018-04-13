7923db9541b197f28c66732ae8db594c6b2c74b9
jimmy <jimmy@qq.com>
2018-04-14 02:57:28 +0800

    0414

--- src/main/java/redis/TestTransaction.java
+++ src/main/java/redis/TestTransaction.java
@@ -14,14 +14,16 @@ public class TestTransaction {
     public static void main(String[] args) throws Exception {
 
        // Jedis jedis = new Jedis("192.168.241.1", 6379);
-        Jedis jedis = new Jedis("127.0.0.1", 6379);
-        Transaction tx = jedis.multi();
-
-        Thread.sleep(7000);
-        tx.set("k1", "9999");
-        tx.set("k2", "8888");
-
-        System.out.println(tx.exec());;
+        Jedis jedis = new Jedis("10.10.10.238", 6379);
+        jedis.auth("123456");
+        System.out.println(jedis.ping());
+//        Transaction tx = jedis.multi();
+//
+//        Thread.sleep(7000);
+//        tx.set("k1", "9999");
+//        tx.set("k2", "8888");
+//
+//        System.out.println(tx.exec());;
 
 
     }
--- src/main/java/redis/test/TestPing.java
+++ src/main/java/redis/test/TestPing.java
@@ -5,7 +5,8 @@ import redis.clients.jedis.Jedis;
 public class TestPing {
 	public static void main(String[] args) 
 	{
-		Jedis jedis = new Jedis("127.0.0.1",6379);
+		Jedis jedis = new Jedis("192.168.141.133",6379);
 		System.out.println(jedis.ping());
+		System.out.println(jedis.get("k"));
 	}
 }
