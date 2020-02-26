22d8736c5b3078bebad03f79c802240855b5c450
jimmy <jimmy723@qq.com>
2020-02-26 14:37:58 +0800

    2

--- build/classes/java/main/generated/jmh/test/generated/JMH02_jmhType.java
+++
@@ -1,4 +0,0 @@
-package jmh.test.generated;
-public class JMH02_jmhType extends JMH02_jmhType_B3 {
-}
-
--- build/classes/java/main/generated/jmh/test/generated/JMH03_jmhType.java
+++
@@ -1,4 +0,0 @@
-package jmh.test.generated;
-public class JMH03_jmhType extends JMH03_jmhType_B3 {
-}
-
