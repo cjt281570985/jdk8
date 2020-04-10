5c80e89fbd0e469d036eadb3e6fa54910657e76f
jimmy <jimmy723@qq.com>
2020-04-10 14:53:32 +0800

    0410

--- src/test/java/T.java
+++ src/test/java/T.java
@@ -1,32 +1,17 @@
 import com.google.common.base.CharMatcher;
 import com.google.common.base.MoreObjects;
 import com.google.common.base.Strings;
+import java.util.regex.Pattern;
 import lombok.Data;
 
 public class T {
 
+    public static final String REGEX_MOBILE_EXACT = "^1[3-9]\\d{9}$";
+
     public static void main(String[] args) {
 
-        String string = "hello-world-java";
-        String str = "";
-
-        System.out.println(Strings.isNullOrEmpty(string));
-        System.out.println(Strings.isNullOrEmpty(str));
-        System.out.println(Strings.commonSuffix("cjt001a", "cj990001"));
-        System.out.println(Strings.repeat("19",3));
-
-        System.out.println(Strings.padEnd("cjt",5, '0'));
-        System.out.println(Strings.padStart("cjt",3, '0'));
-
-        System.out.println("-----------------------");
-
-        System.out.println(CharMatcher.inRange('0','9').matches('a'));
-        System.out.println(CharMatcher.inRange('0','9').matches('3'));
-
-        System.out.println("------33-----------------");
-        Student2 student = new Student2();
-        student.setId(99);
-        System.out.println(MoreObjects.toStringHelper(student).addValue(3).add("k","v"));;
+        boolean matches = Pattern.matches(REGEX_MOBILE_EXACT, "13929298882");
+        System.out.println(matches);
 
     }
 
